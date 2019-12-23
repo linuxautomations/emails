9dddff2ef2d09f3c9450d73a99b4d32c26e536b0
KalluruS <>
2019-12-23 14:27:17 +0530

    --- Auto-Git Commit ---

--- list.sh
+++ list.sh
@@ -3,4 +3,5 @@
 [ -z "$1" ] && echo "Enter Batch File as Input" && exit 1
 [ -n "$2" ] && TYPE=TRAINING || TYPE=VIDEO
 
-cat $1.txt | grep $TYPE | grep -Ev '^'
+
+cat $1.txt | grep $TYPE | grep -Ev '^$|^#' 
